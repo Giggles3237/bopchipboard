b09e22a6b7d379243b1ba9c678be7aa5c5525454
Giggles3237 <chris.lasko@gmail.com>
2024-11-06 20:14:58 -0500

    debugging 2.0

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://bopchips.netlify.app', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Import routes
